dbf69cb62bb3757899b0c238d4662890b7f71e9e
divya <divya.vegesna55@gmail.com>
2024-05-19 17:34:42 +0530

    finl

---
+++ workstation.tf
@@ -0,0 +1,57 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "workstation"
+  ami =   data.aws_ami.centos8.id
+  instance_type          = "t2.micro"
+#   key_name               = "user1"
+#   monitoring             = true
+  vpc_security_group_ids = [aws_security_group.allow_eks.id]
+  subnet_id              = "subnet-0eb2e6a76b1b79278"
+  user_data = file("workstation.sh")
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_security_group" "allow_eks" {
+  name        = "allow_eks"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+    ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent      = true
+  owners           = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Centos-8-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
